9948381d4fdc25a71a6fbd57609b70571bfe6442
khanumar03 <khanumar28122003@outlook.in>
2023-08-26 01:07:47 +0530

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setdata] = useState(null)
+
+  async function get() {
+    await fetch("http://localhost:5000/api").then(async (saved) => {
+      const msg = await saved.json()
+      setdata(msg.message)
+    })
+    .catch((err) => console.log(err))
+  }
+
+  useEffect(() => {
+    get()
+  },[])
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React - {data}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+
+app.use(express.json())
+app.use(cors({origin: "*"}))
+
+app.get("/api", (req,res) => {
+    return res.json({message: "working"})
+})
+
+app.listen(5000, () => console.log("server connected"))
